c3862b2804d9ed452ae0c33c7bcaeca9e5e969da
Olein-jp <koji.kuno@gmail.com>
2022-09-09 13:30:12 +0900

    react-feather icon 追加

--- src/components/TableLink.js
+++ src/components/TableLink.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { ExternalLink } from "react-feather";
+
 const TableLink = (props) => {
   return (
     <Link href={props.url} passHref>
-      <a className="underline" target="_blank" rel="noopener noreferrer">
-        {props.url}
+      <a
+        className="underline flex justify-center"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <ExternalLink size="16" />
       </a>
     </Link>
   );
